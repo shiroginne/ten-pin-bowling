25281818f4ad1d01528899e80dbf91d07d02ee3b
Evgeny Sugakov <shiroginne@gmail.com>
2018-03-03 10:22:01 +0100

    Move controller actions to consern
    As DHH sugested.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,3 @@
 class ApplicationController < ActionController::API
-  rescue_from ActiveRecord::RecordNotFound, with: :render_404
-  rescue_from ActionController::UnknownFormat, with: :unsupported_format
-
-  private
-    def render_404(exception = nil)
-      logger.debug "Rendering 404: #{exception.message}" if exception
-      render "shared/404", status: 404
-    end
-
-    def unsupported_format(exception = nil)
-      logger.debug "Rendering 404: #{exception.message}" if exception
-      render plain: I18n.t("unsupported_format"), status: 415, layout: false
-    end
-
-    def render_json_error(errors)
-      render json: { errors: errors, code: 422, status: 422 }, status: :unprocessable_entity
-    end
+  include ResponseHandling
 end
